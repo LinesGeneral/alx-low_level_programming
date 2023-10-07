08d94fd0662a873abbedc5b57980f779d631ce83
Lines General <inaloegiga@gmail.com>
2023-10-07 04:07:39 +0000

    variable if,else,while test

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* void - empty argument
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alph[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alph[i]);
+	}
+	putchar('\n');
+	return (0);
+}
