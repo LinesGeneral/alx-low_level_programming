a31944954cecec9cc85148a070ed850ffec1a33e
Emmanuel Inalo <inaloegiga@gmail.com>
2023-10-10 23:00:33 +0100

    all good changes committed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
 * main - Entry point for function
-* @void - no parameter
+*
 * Return: Always 0 (success)
 */
-int main()
+void print_alphabet(void)
 {
 	char alph;
+
 	for (alph = 'a'; alph <= 'z'; alph++)
 	{
 		_putchar(alph);
 	}
 	_putchar('\n');
-} 
+}
