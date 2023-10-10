262c361aeed2111000bc11be2520026db867708d
Emmanuel Inalo <inaloegiga@gmail.com>
2023-10-10 22:27:19 +0100

    entry level c programs

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - Entry point for function
+* @void - no parameter
+* Return: Always 0 (success)
+*/
+int main()
+{
+	char alph;
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+	_putchar("\n");
+} 
