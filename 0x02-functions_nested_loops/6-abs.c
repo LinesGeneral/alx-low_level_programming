d0539211ca90efc1f8bf828781b34db10c1632db
Emmanuel Inalo <inaloegiga@gmail.com>
2023-10-11 13:28:49 +0100

    we try

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _abs - prints absolute value of a number
+* @i: if i takes a negative value
+* Return: Always 0 (Success)
+*/
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = (-1) * i;
+	}
+}
