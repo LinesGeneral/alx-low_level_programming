8c4d3feda63d6a74b5aef7e857a0d0d6074a4de0
Emmanuel Inalo <inaloegiga@gmail.com>
2023-10-13 22:41:14 +0100

    more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	int new;
+	new = mul(5, 7);
+
+	printf("the answer is %d\n", new);
+	return 0;
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* mul - function multiplies two integers a and b
+* @a: 1 of 2 integers to be multiplied
+* @b: 1 of 2 integers to be multiplied
+* Return: integer multiplication
+*/
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
