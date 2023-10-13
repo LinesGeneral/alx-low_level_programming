0cc55f3b5d15a32942859172a216ebcc2e97356e
Emmanuel Inalo <inaloegiga@gmail.com>
2023-10-13 01:23:43 +0100

    coding is hard

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-* _abs - prints absolute value of a number
-* @i: if i takes a negative value
+* _abs - function that computes the absolute value
+*
+* @i: take input value for the function
+*
 * Return: Always 0 (Success)
 */
 int _abs(int i)
 {
 	if (i < 0)
-	{
 		i = (-1) * i;
-	}
+
+		return (i);
 }
