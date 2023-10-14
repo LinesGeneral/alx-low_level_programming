194a00bf6041ee3a06c20201dc122a0d48e4dbd7
Emmanuel Inalo <inaloegiga@gmail.com>
2023-10-14 11:10:21 +0100

    more fuctions and nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/** 
+* main - print numbers 1 - 100
+*
+* Return: Always 0
+*/
+
+int main()
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		printf("FizzBuzz ");
+		else if ( i % 5 == 0)
+		printf("Buzz ");
+		else if (i % 3 == 0)
+		printf("Fizz ");
+		else
+		printf("%d ", i);
+	}
+}	
