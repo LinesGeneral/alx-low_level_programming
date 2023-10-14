a253259bbbdfddab855ba33f9aae51f3f447f245
Emmanuel Inalo <inaloegiga@gmail.com>
2023-10-14 08:45:08 +0100

    more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_line - function draws a straight line in the terminal
+* @n: parameter passed to function
+* Return: no return
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n == 0 || n < 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('_');
+	}
+	_putchar('\n');
+}
