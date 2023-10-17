e8eb536a8e0b200befeaa7a595113eadab964c22
Emmanuel Inalo <inaloegiga@gmail.com>
2023-10-17 12:13:23 +0100

    pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts - function prints a string, followed by new line
+* @str: required parameter
+* Return: Always 0.
+*/
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (count == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
+	}
+}
