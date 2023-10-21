c2a506dd49b74450eaf77e844e1d73e04e90d936
Emmanuel Inalo <inaloegiga@gmail.com>
2023-10-21 13:29:24 +0100

    variables if, else and while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,24 +5,27 @@
  * prints only the smallest combination
  * Return: Always 0.
 */
-int main()
+int main(void)
 {
-    int a, b;
+	int a, b;
 
-    for (a = 48; a < 58; a++)
-    {
-        for (b = 48; b < 58; b++)
-        {
-            if (a != b && a < b)
-            {
-                putchar(a);
-                putchar(b);
-                putchar(',');
-                putchar(' ');
-            }
-        }
-        
-    }
-    putchar('\n');
-    return (0);
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 48; b < 58; b++)
+		{
+			if (a != b && a < b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a == 56 && b == 57)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
