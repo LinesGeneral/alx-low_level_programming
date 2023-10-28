57fa850f123d5f8ad0d83454b9144af3ea4614cf
Emmanuel Inalo <inaloegiga@gmail.com>
2023-10-28 12:48:59 +0100

    passing arguments to main

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - program prints filename 
-* @argc - counts the number of arguments
-* @argv - an array of arguments passed on the command line
+* main - program prints filename
+* @argc: counts the number of arguments
+* @argv: an array of arguments passed on the command line
 * Return: Always 0.
 */
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-}	
+}
