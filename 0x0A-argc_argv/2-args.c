fde83819fecb1e20f11b61975bd35ed99536bb8a
Emmanuel Inalo <inaloegiga@gmail.com>
2023-10-28 14:08:00 +0100

    passing arguments to main

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - program prints all arguments it receives
+* @argc: parameter is used for argument count
+* @argv: parameter shows strings passed on the command line
+* Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+	int count;
+
+	if (argc >= 0)
+	{
+		for (count = 0; count < argc; count++)
+		{
+			printf(" %s\n", argv[count]);
+		}
+	}
+	return (0);
+}
