7b85aa56c9e004fd7b97bcee66fee29bb954157d
Emmanuel Inalo <inaloegiga@gmail.com>
2023-11-06 13:53:00 +0100

    structures and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+* struct dog - new struct type
+* @name: First member
+* @age: Second member
+* @owner: Third member
+*
+* Description: this struct takes name, age and owneer of the dog
+*/
+
 struct dog
 {
 	char *name;
