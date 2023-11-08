2443ece2fda3e94c2b2f5bbf792dfa86e1aeb96f
Emmanuel Inalo <inaloegiga@gmail.com>
2023-11-08 20:11:13 +0100

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
+#include <ctype.h>
 /**
 * print_name - function prints a name
 * @name: first parameter
@@ -21,4 +21,21 @@ void print_name(char *name, void (*f)(char *))
 void print_default(char *name)
 {
 	printf("%s\n", name);
+}
+/**
+* print_upcase - function prints in uppercase letters
+* @name: first parameter
+*
+* Return: no return(void function)
+*/
+void print_upcase(char *name)
+{
+	char *uppercase_name = name;
+	int i;
+
+	for (i = 0; name[i] != '\0'; i++)
+	{
+		uppercase_name[i] = toupper(name[i]);
+	}
+	printf("%s\n", uppercase_name);
 }
